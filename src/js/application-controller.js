93ad4096da62bb79553e8b063db00fed427922a3
kamrenz <kamren@developintelligence.com>
2015-07-15 09:51:59 -0400

    Adding info on $scope

--- src/js/application-controller.js
+++ src/js/application-controller.js
@@ -5,15 +5,38 @@
       .controller('AppController', AppController);
 
     /** 
-     * Main Controller for the application.
+     * Main Controller JavaScript Constructor for the application.
      **/
     function AppController() {
 
         //Saving the this reference
-        var ap = this;
+        var ac = this;
 
         //Adding a property to the Controller instance
-        ap.place = 'World';
+        ac.place = 'World';
     }
+
+
+    ///////////////////////////////////////
+    /**
+     * Controller-as syntax is a newer syntax to Angular.
+     *   Some Angular applications rely heavily on $scope instead.
+     *
+     *   The move to controller-as syntax will help in the transition
+     *      to Angular 2.0
+     *
+     *   Below is how the above is done utilizing $scope instead of
+     *     controller-as syntax
+     */
+    //angular.module('hello')
+    //    //$scope is being Dependency Injected into the Constructor
+    //    .controller('AppController', ['$scope', AppController]);
+    //
+    //function AppController($scope) {
+    //    //Within the index.html we would reference this variable simply
+    //    //  as place rather than ac.place because the $scope is the
+    //    //  glue between the view and the controller
+    //    $scope.place = 'World';
+    //}
 
 })();
